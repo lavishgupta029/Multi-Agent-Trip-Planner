c19a019213d60df84217083d2210f7493dfa00fd
lavish gupta <lavish.gupta@ibm.com>
2026-09-16 12:20:46 +0530

    flight and tavily tools added

---
+++ test.py
@@ -0,0 +1,8 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+
+res= search_flights("Plan a 7 days Nepal trip from India")
+
+# res= tavily_search("Best hotels in india")
+
+print(res)
