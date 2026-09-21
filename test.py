928cc0fcbd15e042e79fefaa6e5a0089a459d153
lavish gupta <lavish.gupta@ibm.com>
2026-09-21 23:52:47 +0530

    app added

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8", errors="replace")
+
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
-res= search_flights("Plan a 7 days Nepal trip from India")
+# res = tavily_search("Best hotels in India")
+# print(res)
 
-# res= tavily_search("Best hotels in india")
 
-print(res)
+# res = search_flights("Plan a 4 days trip from Pune to Hyderabad")
+# print(res)
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
